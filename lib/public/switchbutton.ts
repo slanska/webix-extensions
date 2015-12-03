621b57d97da095015478c5fabb9f197f45f670ff
slanska <slan_ska@yahoo.com>
2015-12-03 08:34:25 -0500

    changes in switchbutton

--- lib/public/switchbutton.ts
+++ lib/public/switchbutton.ts
@@ -8,74 +8,55 @@
  Renders as input checkbox with ionic framework styling
 
  Supported color themes:
-     positive
-     calm
-     assertive
-     energized
-     stable
-     light
-     balanced
-     royal
-     dark
+ positive
+ calm
+ assertive
+ energized
+ stable
+ light
+ balanced
+ royal
+ dark
  */
 
 webix.protoUI(
     {
         name: "switchbutton",
-        defaults: {width: 200, height: 40, small: true, theme: "stable", disabled: "", checked: "", borderless: true},
+        defaults: {
+            width: 200, height: 40, small: false, theme: "light",
+            borderless: true,
+
+            template: function (config, common)
+            {
+                console.log(`e ${config.toString()}, obj ${common.toString()}`);
+
+                var id = config.name || "x" + webix.uid();
+
+                var checked = (config.checkValue == config.value);
+                var html =
+                    `
+                    <label class='toggle disable-user-behavior ${this.small === true ? "toggle-small" : ""} toggle-${this.theme}'>
+                    <input value='on'
+                    type='checkbox' ${this.disabled} ${checked ? 'checked' : ''}> <div class='track'> <div class='handle'></div></div>${config.labelRight || ''}</label>`;
+
+                return common.$renderInput(config, html, id);
+            }
+        },
+
         $init: function (config)
         {
-            this.small = config.small || true;
-            this.theme = config.theme || "stable";
-            this.width = config.width || 200;
-            this.height = config.height || 40;
-            this.checked = config.checked;
-            this.disabled = config.disabled;
-
-            this.captchaImageID = webix.uid().toString();
-            this.refreshButtonID  = webix.uid().toString();
-            this.$view.innerHTML =
-                `<label class='toggle disable-user-behavior ${this.small === true ? "toggle-small" : ""} toggle-${this.theme}'>` +
-                `<input value='on' class='ng-untouched ng-valid ng-dirty ng-valid-parse'` +
-                `type='checkbox' ${this.disabled} ${this.checked}> <div class='track'> <div class='handle'></div> </div> </label>`;
+            this.small = config.small || false;
+            this.theme = config.theme || "light";
         },
 
-
-        render: function ()
-        {
-
-
-        },
-
-        $setSize: function (x:number, y:number)
-        {
-
-        },
-
-        $getSize: function ()
-        {
-            // Array: minWidth, maxWidth, minHeight, maxHeight
-            //return [-1, -1, -1, -1];
-            return [60, -1, 40, -1];
-        },
-
-        refreshValue: function ()
-        {
-
-        },
-
-        setValue: function (value)
-        {
-        },
-
-        getValue: function ()
-        {
-        },
-
-        focus: function ()
-        {
+        on_click: {
+            "cmn-toggle": function (e, obj, node)
+            {
+                console.log(`e ${e.toString()}, obj ${obj.toString()}, node ${node.toString()}`);
+                //this.toggle();
+            }
         }
     },
 
-    webix.ui.view
+    webix.ui.checkbox
 );
